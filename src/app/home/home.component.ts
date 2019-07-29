4f2244bff7760b4167e1e13714d91c49e2f3ac39
ekwok <eric.kwoq@gmail.com>
2019-07-29 15:05:50 -0700

    redirect to genetic component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
   }
 
   signIn() {
-    window.location.href = 'https://api.23andme.com/authorize/?redirect_uri=https://genomex.stackblitz.io/results/&response_type=code&client_id=5a1f0ce948126c290b1f6b10ecba07fe&scope=basic names report:all genomes phenotypes:read:all';
+    window.location.href = 'https://api.23andme.com/authorize/?redirect_uri=https://genomex.stackblitz.io/results/genetic/&response_type=code&client_id=5a1f0ce948126c290b1f6b10ecba07fe&scope=basic names report:all genomes phenotypes:read:all';
   }
 
 }
